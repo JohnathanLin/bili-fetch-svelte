a813ff267ee21a41d094f8c1eff7fb7029e6d375
Johnathan Lin <289348588@qq.com>
2024-06-08 21:08:11 +0800

    vercel启动

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-vercel';
-
+import { vitePreprocess } from '@sveltejs/kit/vite';
 export default {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
 			// see below for options that can be set here
